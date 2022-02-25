24808710ae2089ca6b1ab39e30f403a5bd9ea361
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-02-26 00:12:23 +0100

    :tada: Begin the Ends project

---
+++ bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+const APP_NAME = 'Ends';
+
+return require_once __DIR__ . "/php_packages/autoload.php";
---
+++ client/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bootstrap.php';
+
+echo '<p>Running <b>' . APP_NAME . '</b> from CGI at ' . date('Y-m-d H:i:s') . '</p>';
